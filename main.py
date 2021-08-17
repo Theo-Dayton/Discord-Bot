5bd6a6b5ec8e7ed5ca4a076f71118624c4887022
Theo-Dayton <69463213+Theo-Dayton@users.noreply.github.com>
2021-08-17 19:29:54 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+from discord.ext import commands
+
+client = commands.Bot(command_prefix='!')
+
+@client.event
+async def on_ready():
+    print('client ready')
+
+@client.command()
+async def ping():
+    await client.say('Pong')
+
+@client.event
+async def on_message(message):
+    if client.user.id != message.author.id:
+        if 'foo' in message.content:
+            await client.send_message(message.channel, 'bar')
+
+    await client.process_commands(message)
+
+client.run('TOKEN')
